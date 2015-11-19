a844ba3deda49e2c6e051b28017527efa6d66894
Richard Chang <Richard Chang>
2015-11-19 14:30:44 -0800

    whiteboard example

---
+++ Whiteboard/Assets/HandRaycast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kinect = Windows.Kinect;
+
+public class HandRaycast : MonoBehaviour {
+    private GameObject leftWrist;
+    private GameObject rightWrist;
+    private GameObject leftHand;
+    private GameObject rightHand;
+
+    // Use this for initialization
+    void Start () {
+        leftWrist = SkeletonRender.body.transform.FindChild(Kinect.JointType.WristLeft.ToString()).gameObject;
+        rightWrist = SkeletonRender.body.transform.FindChild(Kinect.JointType.WristRight.ToString()).gameObject;
+        leftHand = SkeletonRender.body.transform.FindChild(Kinect.JointType.HandLeft.ToString()).gameObject;
+        rightHand = SkeletonRender.body.transform.FindChild(Kinect.JointType.HandRight.ToString()).gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Physics.Raycast(leftHand.transform.position, leftHand.transform.position - leftWrist.transform.position, out hit)) {
+
+        }
+	}
+}
---
+++ Whiteboard/Assets/Kinect/WhiteboardExample.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhiteboardExample : MonoBehaviour
+{
+    void Start()
+    {
+        Texture2D texture = new Texture2D(128, 128);
+        GetComponent<Renderer>().material.mainTexture = texture;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+           
+                Renderer rend = hit.transform.GetComponent<Renderer>();
+                MeshCollider meshCollider = hit.collider as MeshCollider;
+                Texture2D tex = rend.material.mainTexture as Texture2D;
+                Vector2 pixelUV = hit.textureCoord;
+                pixelUV.x *= tex.width;
+                pixelUV.y *= tex.height;
+                tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.red);
+                tex.Apply();
+
+            }
+                
+        }
+    }
+}
