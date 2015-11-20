f44c8aafd14488d597d8c677cae625aed370f32a
onion-ring <alex.ringlein@gmail.com>
2015-11-19 19:16:08 -0500

    fb

---
+++ WindowsKinect/Assets/Scripts/FacebookLogIn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+
+public class FacebookLogIn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FB.LogInWithReadPermissions (
+			new List<string>(){"public_profile", "email", "user_friends"},
+		AuthCallback
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
