b81aa8f431b6c36268d2272f20f4e2b40a830ddd
onion-ring <alex.ringlein@gmail.com>
2015-11-19 23:23:49 -0500

    test

---
+++ WindowsKinect/Assets/manage.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class manage : MonoBehaviour {
+	
+	public Text i;
+	private string save;
+	public GameObject manager;
+
+	// Use this for initialization
+	void Start () {
+		save = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string val = i.text;
+		if (save != val) {
+			GameObject.Destroy (GameObject.Find ("Manager(Clone)"));
+			GameObject.Instantiate (manager);
+			save = val;
+		}
+	}
+}
