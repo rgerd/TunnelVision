1091486021e07f35ded090e609e5dc0488f9c57c
Robert Gerdisch <rgerdisch@gmail.com>
2015-11-19 20:52:24 -0500

    MainScene

--- MainAndroid/Assets/Scripts/Foundation/Kinect/BoneScript.cs
+++ MainAndroid/Assets/Scripts/Foundation/Kinect/BoneScript.cs
@@ -9,6 +9,8 @@ public class BoneScript : MonoBehaviour {
 		Vector3 midpoint = (joint1.transform.position + joint2.transform.position) / 2;
 		transform.position = midpoint;
 		float dist = Vector3.Distance (joint1.transform.position, joint2.transform.position) / 2;
+		if (dist == 0)
+			return;
 		transform.localScale = new Vector3(radius, dist, radius);
 		transform.localRotation = Quaternion.LookRotation (joint2.transform.position - joint1.transform.position);
 		transform.Rotate (new Vector3 (90, 0, 0));
